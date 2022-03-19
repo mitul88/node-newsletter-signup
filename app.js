90116ea7a01c10d1da6f829bf266e2ebe5672586
shahed <shahed.rahman88@gmail.com>
2022-03-19 18:16:34 +0600

    bootstrap html template added

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require('express');
 
 const app = express();
 
+app.get("/", (req, res)=> {
+    res.sendFile(__dirname + "/signup.html");
+})
+
 
 app.listen(3000, ()=> {
     console.log("Server is up and running in port 3000!")
