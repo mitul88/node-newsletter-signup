b4e73e723f031ce1c1518dcc2edd85781e86378e
shahed <shahed.rahman88@gmail.com>
2022-03-20 20:57:16 +0600

    added bodyparser

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 
 const app = express();
 
-app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static("public"));
 
 app.get("/", (req, res)=> {
     res.sendFile(__dirname + "/signup.html");
